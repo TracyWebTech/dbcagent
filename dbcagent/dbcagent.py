2d60fd1ee264ff7c061639d663fee505bcc6342a
Alexandre Barbosa <alexandreab@live.com>
2015-04-19 11:54:04 -0300

    Read configurations from files. Closes #5

--- dbcagent/dbcagent.py
+++ dbcagent/dbcagent.py
@@ -1,6 +1,7 @@
-from time import sleep
 import json
 import requests
+from time import sleep
+from ConfigParser import SafeConfigParser
 
 from slavedb import SlaveDB
 from db import DB
@@ -21,10 +22,19 @@ def compare(slave):
 
 if __name__ == '__main__':
 
+    conf_file = '/tmp/dbcagent_config.ini'
+    parser = SafeConfigParser()
+    parser.read(conf_file)
+
     host = '10.10.10.3'
     user = 'replicator'
     password = '123456'
     db = 'dbexample'
+
+    host = parser.get('slave','host')
+    user = parser.get('slave','user')
+    password = parser.get('slave','password')
+    db = parser.get('slave','database')
 
     slave = SlaveDB(host, user, password, token='bananabananabanana')
     slave.insert_db(db)
@@ -32,25 +42,19 @@ if __name__ == '__main__':
     slave.read_master_info()
 
     while True:
-        print(".")
-        sleep(1)
         comp_list = compare(slave)
         json_comp = json.dumps(comp_list, sort_keys=True)
         print("Status: {}".format(json_comp))
-#        sleep(5)
-#        url = "http://10.10.10.2:8000/monitor/send-status"
-#        payload = json_comp
-#
-#        try:
-#            headers = {'Content-type': 'application/json'}
-#
-#            r = requests.post(url, data=payload, headers=headers)
-#
-#            print("\n\tSever status: {}\n".format(r.status_code))
-#
-#            if r.status_code==500:
-#                print(r.text)
-#                break
-#
-#        except Exception as e:
-#            print("Erro {}".format(str(e)))
+        sleep(5)
+        url = "http://10.10.10.2:8000/monitor/send-status"
+        payload = json_comp
+
+        try:
+            headers = {'Content-type': 'application/json'}
+
+            r = requests.post(url, data=payload, headers=headers)
+
+            print("\n\tSever status: {}\n".format(r.status_code))
+
+        except Exception as e:
+            print("Erro {}".format(str(e)))
